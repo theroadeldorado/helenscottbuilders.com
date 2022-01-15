ee8c8abe0eb27129a8e9d467999ffb28e0a7ef5f
John Young <theroad2eldorado@gmail.com>
2022-01-14 21:05:40 -0500

    💄 updates fonts

--- wp-content/themes/fire/theme/assets/scripts/components/index.js
+++ wp-content/themes/fire/theme/assets/scripts/components/index.js
@@ -2,6 +2,5 @@ export * from './component';
 export * from './component-record';
 export * from './convert-to-inline-svg';
 export * from './detect';
-export * from './lazy-loader';
 export * from './social-share';
 export * from './storage';
